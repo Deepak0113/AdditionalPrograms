8a804467727e467725108292c15873203dd0910a
Deepak0113 <mdeecreations@gmail.com>
2023-01-13 09:50:52 +0530

    commit

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ContainerWithMostWater {
+class ContainerWithMostWater {
+    static int maxArea(int[] height) {
+        int volume = 0;
+        int left = 0, right = height.length-1;
+
+        while(left<right){
+            int diff = (right-left)*Math.min(height[left],height[right]);
+            if(volume<diff) volume = diff;
+            if(height[left] < height[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(volume);
+
+        return volume;
+    }
+
+    public static void main(String[] args) {
+        int[] testInput = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(testInput));
+    }
 }
