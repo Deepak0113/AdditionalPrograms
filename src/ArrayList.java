7f0a0c5141f6a580071bc19c0fe6fe481c97b764
Deepak0113 <mdeecreations@gmail.com>
2023-01-14 13:59:26 +0530

    ArrayList

---
+++ src/ArrayList.java
@@ -0,0 +1,59 @@
+class ArrayEmptyException extends Exception{
+    ArrayEmptyException(){
+        super("Empty ArrayList");
+    }
+}
+
+public class ArrayList {
+    // default size 5
+    private int[] array = new int[5];
+    private int size = 0;
+
+
+    // add
+    void add(int value){
+        if (size >= array.length) {
+            int[] temp = array.clone();
+            array = new int[temp.length + 5];
+
+            for (int i = 0; i < size; i++) {
+                array[i] = temp[i];
+            }
+        }
+
+        array[size] = value;
+        size++;
+    }
+
+    // delete
+    void delete(){
+        try{
+            if(size == 0)
+                throw new ArrayEmptyException();
+            if(size%5 == 1){
+                int[] temp = array.clone();
+                array = new int[size-1];
+                for(int i=0; i<size-1; i++){
+                    array[i] = temp[i];
+                }
+            }
+            size--;
+        } catch(ArrayEmptyException aee){
+            System.out.println(aee);
+        }
+    }
+
+    // display
+    void display(){
+        for(int i=0; i<size; i++){
+            System.out.print(array[i]+" ");
+        }
+
+        System.out.println(" Size: "+size+" Length: "+array.length);
+    }
+
+    // size
+    int getSize(){
+        return size;
+    }
+}
